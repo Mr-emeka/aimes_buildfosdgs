dcf6c3639c13709a709ecba0ad9faad570dad333
Mr-emeka <speak2c.emeka@gmail.com>
2020-04-11 14:01:07 +0100

    changed estimator file for FE

--- buildforsdgs_frontend/src/estimator.js
+++ buildforsdgs_frontend/src/estimator.js
@@ -1,16 +1,13 @@
 const formatDate = (periodType, timeToElapse) => {
   let days;
-  if (/weeks/i.test(periodType)) {
-    const value = (timeToElapse * 7) / 3;
-    days = Math.trunc(value);
+  if (/week/i.test(periodType)) {
+    days = Math.trunc((timeToElapse * 7) / 3);
   }
-  if (/months/i.test(periodType)) {
-    const value = (timeToElapse * 30) / 3;
-    days = Math.trunc(value);
+  if (/month/i.test(periodType)) {
+    days = Math.trunc((timeToElapse * 30) / 3);
   }
   if (/days/i.test(periodType)) {
-    const value = (timeToElapse / 3);
-    days = Math.trunc(value);
+    days = Math.trunc(timeToElapse / 3);
   }
   return days;
 };
@@ -24,6 +21,7 @@ const covid19ImpactEstimator = (data) => {
 
   impact.currentlyInfected = data.reportedCases * 10;
   severeImpact.currentlyInfected = data.reportedCases * 50;
+
   impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** requestedTime);
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** requestedTime);
 
@@ -31,15 +29,15 @@ const covid19ImpactEstimator = (data) => {
   impact.severeCasesByRequestedTime = (15 / 100) * impact.infectionsByRequestedTime;
   severeImpact.severeCasesByRequestedTime = (15 / 100) * severeImpact.infectionsByRequestedTime;
 
-  impact.hospitalBedsByRequestedTime = Math.trunc((35 * 100) * data.totalHospitalBeds - impact.severeCasesByRequestedTime);
-  severeImpact.hospitalBedsByRequestedTime = Math.trunc((35 * 100) * data.totalHospitalBeds - severeImpact.severeCasesByRequestedTime);
+  impact.hospitalBedsByRequestedTime = Math.trunc((35 / 100) * data.totalHospitalBeds - impact.severeCasesByRequestedTime);
+  severeImpact.hospitalBedsByRequestedTime = Math.trunc((35 / 100) * data.totalHospitalBeds - severeImpact.severeCasesByRequestedTime);
 
   // Challenge 3
-  impact.casesForICUByRequestedTime = Math.trunc((5 * 100) * impact.infectionsByRequestedTime);
-  severeImpact.casesForICUByRequestedTime = Math.trunc((5 * 100) * severeImpact.infectionsByRequestedTime);
+  impact.casesForICUByRequestedTime = Math.trunc((5 / 100) * impact.infectionsByRequestedTime);
+  severeImpact.casesForICUByRequestedTime = Math.trunc((5 / 100) * severeImpact.infectionsByRequestedTime);
 
-  impact.casesForVentilatorsByRequestedTime = Math.trunc((2 * 100) * impact.infectionsByRequestedTime);
-  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc((2 * 100) * severeImpact.infectionsByRequestedTime);
+  impact.casesForVentilatorsByRequestedTime = Math.trunc((2 / 100) * impact.infectionsByRequestedTime);
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc((2 / 100) * severeImpact.infectionsByRequestedTime);
 
   impact.dollarsInFlight = Math.trunc((impact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / days);
   severeImpact.dollarsInFlight = Math.trunc((severeImpact.infectionsByRequestedTime * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / days);
