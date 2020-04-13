f2870ca8f99ffeba439be6f9a741c5974da7fa6f
Mr-emeka <speak2c.emeka@gmail.com>
2020-04-13 11:32:34 +0100

    change from react to static html

---
+++ buildforsdgs_frontend/build/precache-manifest.c84f6e0929e30dc10f5b33770bb6934c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de14d9c1d9920e5d2fa02926de4bb662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42add029b1a11b21d1f8",
+    "url": "/static/css/2.9cc29b39.chunk.css"
+  },
+  {
+    "revision": "d943de1d3ccf13e181e3",
+    "url": "/static/css/main.fda9d134.chunk.css"
+  },
+  {
+    "revision": "42add029b1a11b21d1f8",
+    "url": "/static/js/2.f0374c98.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.f0374c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d943de1d3ccf13e181e3",
+    "url": "/static/js/main.a06e9582.chunk.js"
+  },
+  {
+    "revision": "e603df780906598969a3",
+    "url": "/static/js/runtime-main.f59fe97b.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "3f3a653980dcf0cc657e9b3548b49aa8",
+    "url": "/static/media/logo.3f3a6539.jpg"
+  }
+]);
--- buildforsdgs_frontend/src/component/Button/Button.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Button } from 'element-react';
-
-function MyButton({ loading,handleSubmit}) {
-  return <Button data-go-estimate type="primary" onClick={handleSubmit} loading={loading}>Estimate</Button>
-}
-export default MyButton;
